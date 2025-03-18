2bc865f57f35f6f3e3fa7951a11a793d9730f253
DJ-LAMB <Chibusonma Udensi>
2025-03-19 00:41:11 +0100

    Capital Helper Function

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,10 +1,13 @@
+import { capitalizeFirstLetter } from "@/utils/capital";
 export default function PostCard({ post }) {
     return (
       <div className="bg-gray-500 p-6 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow">
         {/* Post Title */}
-        <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
+        <h2 className="text-xl font-semibold mb-2">
+  {capitalizeFirstLetter(post.title)}
+</h2>
         {/* Post Body */}
-        <p className="text-white">{post.body}</p>
+        <p className="text-white">{capitalizeFirstLetter(post.body)}.</p>
       </div>
     );
   }
