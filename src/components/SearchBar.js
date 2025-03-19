ad9c458b6a83025815ac85a8a77c6934a55bf09c
DJ-LAMB <Chibusonma Udensi>
2025-03-19 01:05:32 +0100

    Updated SearchBox Focus

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,7 @@ export default function SearchBar({ searchTerm, setSearchTerm }) {
       placeholder="Search posts..."
       value={searchTerm}
       onChange={(e) => setSearchTerm(e.target.value)}
-      className=" text-gray-800 w-full p-3 mb-8 border border-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-900"
+      className=" text-gray-900 w-full p-3 mb-8 border border-gray-900 rounded-lg focus:outline-none focus:ring-0 focus:ring-gray-900"
     />
   );
 }
